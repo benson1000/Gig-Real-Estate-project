7a7bdef8f3d23f79f98bbe239f34d77a198562ec
benson1000 <bensonkimani1000@gmail.com>
2022-12-29 16:20:45 +0300

    styling

--- conn.sql
+++ conn.sql
@@ -10,3 +10,13 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL UNIQUE
 );
+
+--define a new sequence contact
+CREATE SEQUENCE contact_seq;
+
+CREATE TABLE contact(
+    id INTEGER NOT NULL PRIMARY KEY DEFAULT NEXTVAL('contact_seq'),
+    fullname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL
+);
